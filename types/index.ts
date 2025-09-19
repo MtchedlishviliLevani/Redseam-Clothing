37ba832cbd5a2a3cc764a60e31f21d142e24848f
MtchedlishviliLevani <levan.mtchedlishvili.5@iliauni.edu.ge>
2025-09-20 00:30:53 +0400

    feat: add image uploader

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,5 @@
+import { ReactNode } from "react"
+
 type ButtonProps = {
   children: React.ReactNode
   icon?: React.ReactNode
@@ -28,4 +30,64 @@ type Product = {
     available_sizes:null | string[];
 }
 
-export type { ButtonProps,RegisterFormValues,Product }
+type ImageUploaderProps = {
+    name: string;
+    label?: string;
+    className?: string;
+}
+type InputProps = {
+  name: string;
+  type?: "text" | "email" | "password";
+  label: string;
+  required?: boolean;
+  className?: string;
+  rules?: Record<string, unknown>;
+}
+
+type ListingClientProps = {
+    product: {
+        data: Product[]
+        links: {
+            first?: string | null
+            last?: string | null
+            prev?: string | null
+            next?: string | null
+        }
+        meta: {
+            current_page: number
+            from: number
+            to: number
+            total: number
+            per_page: number
+        }
+    }
+    currentPage: number
+    totalPages: number
+}
+
+
+type Brand = {
+  id: number;
+  name: string;
+  image: string;
+};
+
+type ProductDetailInfo = {
+  id: number;
+  name: string;
+  description: string | null;
+  release_year: string;
+  cover_image: string;
+  images: string[];
+  price: number;
+  available_colors: string[];
+  available_sizes: string[];
+  brand: Brand;
+};
+
+type ModalProps  ={
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+export type { ButtonProps,RegisterFormValues,Product,ImageUploaderProps,InputProps,ListingClientProps, ProductDetailInfo,ModalProps }

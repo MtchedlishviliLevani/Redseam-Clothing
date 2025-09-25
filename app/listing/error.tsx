a2ad58b5984dffaa57e44dc39bde9f8f9f458e1b
MtchedlishviliLevani <levan.mtchedlishvili.5@iliauni.edu.ge>
2025-09-25 16:07:44 +0400

    feat: remove some styles

--- app/listing/error.tsx
+++ app/listing/error.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function Error({ error, reset }: Props) {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-50 px-4">
+    <div className="flex items-center justify-center min-h-screen">
       <div className="bg-white shadow-lg rounded-xl p-8 max-w-md text-center">
         <h1 className="text-2xl font-bold text-red-600 mb-4">Failed to load product details</h1>
         <p className="text-gray-700 mb-6">{error.message}</p>
